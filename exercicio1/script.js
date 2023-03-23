9a370f9b46f4cb91a8ef1f253b7cfaf8afe01c8a
Mateus Carvalho <mateuscarvalhodev@outlook.com>
2023-03-23 09:43:35 -0300

    exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,22 @@
+const sacolao = [];
+
+const item1 = {
+    name: 'kiwi',
+    preço: 10,
+    available: false
+}
+
+const item2 = {
+    name: 'banana',
+    price: 2,
+    available: true
+}
+
+const item3 = {
+    name: 'dragonFruit',
+    price: 9,
+    available: true
+}
+
+sacolao.push(item1, item2, item3);
+console.log(sacolao);
